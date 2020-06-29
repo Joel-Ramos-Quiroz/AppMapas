9c8ec62d7dbd8d46eda70bd7fce46309ae776317
joel_ <irvinjoelramosquiroz@gmail.com>
2020-06-29 13:37:10 -0500

    Ejemplo

--- app/src/main/java/com/example/appmapas/MainActivity.kt
+++ app/src/main/java/com/example/appmapas/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        variables()
     }
+/*
+Nuestra Variable
+ */
+private fun variables(){
+    var myVariable="Hola Mundo"
+    println(myVariable)
+}
 }
